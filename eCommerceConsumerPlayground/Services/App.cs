ab47e021b6255152bdb647c047d447c14b73065d
Zenska11 <zenska96@gmx.de>
2024-02-08 16:33:44 +0100

    update code

--- eCommerceConsumerPlayground/Services/App.cs
+++ eCommerceConsumerPlayground/Services/App.cs
@@ -1,4 +1,6 @@
+using eCommerceConsumerPlayground.Models;
 using ECommerceConsumerPlayground.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -9,12 +11,14 @@ public sealed class App : BackgroundService
 {
     private readonly IServiceProvider _serviceProvider;
     private readonly ILogger<App> _logger;
-    
+    //private readonly AppDbContext _context;
+
     /// <summary>
     /// Worker service implementation - running Kafka consumer as loop on execute
     /// </summary>
     /// <param name="serviceProvider"></param>
     /// <param name="logger"></param>
+    /// <param name="context"></param>
     public App(IServiceProvider serviceProvider, ILogger<App> logger)
     {
         _serviceProvider = serviceProvider;
@@ -24,7 +28,14 @@ public sealed class App : BackgroundService
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
         _logger.LogInformation($"Worker App STARTED at: {DateTimeOffset.Now}");
-
+        
+        // if (_context.Database.GetPendingMigrations().Any())
+        // {
+        //     Console.WriteLine("Running migrations");
+        //     _context.Database.Migrate();
+        //     Console.WriteLine("Migrations have been successfull");
+        // }
+        
         // Background service workaround for injecting container as scoped and not default singleton
         using IServiceScope scope = _serviceProvider.CreateScope();
         IWorkerService imWorkerService = scope.ServiceProvider.GetRequiredService<IWorkerService>();
